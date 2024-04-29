88d85bc2a9a664e162f76e44bd50b0ca854bb162
jibuo <168422859+jibuo@users.noreply.github.com>
2024-04-29 12:21:30 +0100

    change port to 3004

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log("server is running on port 3000")
+app.listen(3004, ()=>{
+    console.log("server is running on port 3004")
 });
 module.exports = app;
